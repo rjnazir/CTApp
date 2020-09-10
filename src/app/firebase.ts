c8365b2540b05c26411294375746794f2ecd75eb
Jaona Nazir Raharison <raharison.j.nazir@gmail.com>
2020-09-10 08:17:22 +0300

    Modif auth & firebase 10/09/20

---
+++ src/app/firebase.ts
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyBwarvjvWTN5VpiKfcOOb9KIa5I1pWZN_g",
+    authDomain: "ctapp-sit2020.firebaseapp.com",
+    databaseURL: "https://ctapp-sit2020.firebaseio.com",
+    projectId: "ctapp-sit2020",
+    storageBucket: "ctapp-sit2020.appspot.com",
+    messagingSenderId: "469153357896",
+    appId: "1:469153357896:web:7adfbe8500c175d57db87d",
+    measurementId: "G-XFC6C65LQV"
+  };
+
+  export default config
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  username: string = ""
+  password: string = ""
+
+  constructor(
+    public afAuth: AngularFireAuth,
+    public toastController: ToastController
+  ) { }
+
+  ngOnInit() {
+  }
+
+  async login (){
+    const { username, password } = this
+    try {
+      const res = await this.afAuth.signInWithEmailAndPassword(username, password)
+    } catch (err) {
+      console.log(err)
+      if(err.code === "auth/user-not-found"){
+        console.log("Utilisateur introuvable.")
+        this.errorMail();
+      }
+    }
+  }
+
+  async errorMail() {
+    const toast = await this.toastController.create({
+      message: 'Email ou mot de passe incorrect',
+      duration: 2000,
+      position: 'top'
+    });
+    toast.present();
+  }
+
+}
